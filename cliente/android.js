ca7037dc835255dfa4953cb1ec1b36208dc22202
Roosevelt Padilha <rooseveltpadilha@Mac-mini-de-Torre.local>
2019-10-31 18:46:54 -0300

    First commit

---
+++ cliente/android.js
@@ -0,0 +1,32 @@
+var http = require('http');
+
+var opcoes = {
+  hostname: 'localhost',
+  port: 80,
+  path: '/',
+  method: 'get',
+  headers: {
+    'Accept': 'application/json',
+    'Content-type': 'application/json'
+  }
+}
+/*
+//Content-type
+var html = 'nome=José';
+var json = {nome: 'José'};
+var string_json = JSON.stringify(json);
+*/
+var buffer_corpo_response = [];
+
+var req = http.request(opcoes,  function(res){
+  res.on('data', function(pedaco){
+    buffer_corpo_response.push(pedaco);
+  });
+  res.on('end', function(){
+    var corpo_responde = Buffer.concat(buffer_corpo_response).toString();
+    console.log(corpo_responde);
+  });
+});
+
+//req.write(string_json);
+req.end();
